54b35a5e1e60b87547757a60399638856a548f30
NamSangwon <nsw3265@naver.com>
2025-08-04 19:38:45 +0900

    Initial Commit

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-deploy");
+require("hardhat-tracer");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: [
+        process.env.PRIVATE_KEY,
+        process.env.PRIVATE_KEY_USER1,
+        process.env.PRIVATE_KEY_USER2,
+      ].filter(key => key !== undefined),
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    enabled: true
+  }
+};
