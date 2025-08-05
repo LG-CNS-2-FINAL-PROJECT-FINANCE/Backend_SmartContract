ab27660bbfe2830a46c9d7a4f77e4ce4d72b23f5
NamSangwon <nsw3265@naver.com>
2025-08-06 01:07:12 +0900

    Fix : 배포 스크립트 수정
    + 파일 위치
    + 배포자 변수

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -2,6 +2,7 @@
 const { network, ethers, run } = require("hardhat");
 require("dotenv").config();
 const fs = require("fs");
+const path = require("path");
 
 const router = process.env.SEPOLIA_FUNCTIONS_ROUTER;
 const subscriptionId = BigInt(process.env.CHAINLINK_FUNCTIONS_SUBSCRIPTIONS);
@@ -13,13 +14,12 @@ const investmentApiUrl = process.env.INVESTMENT_API_URL;
 const tradeApiUrl = process.env.TRADE_API_URL;
 
 // 자바스크립트 파일 읽기
-const investmentSourceCodeTemplate = fs.readFileSync("./request_api/investment_source.js", "utf8");
-const tradeSourceCodeTemplate = fs.readFileSync("./request_api/trade_source.js", "utf8");
+const investmentSourceCodeTemplate = fs.readFileSync(path.join(__dirname, '../resource/investment_source.js'), 'utf8');
+const tradeSourceCodeTemplate = fs.readFileSync(path.join(__dirname, '../resource/trade_source.js'), 'utf8');
 
-module.exports = async ({ getNamedAccounts, deployments }) => {
+module.exports = async ({ deployments }) => {
     const { deploy, log } = deployments;
-    const { deployer } = await getNamedAccounts();
-    const [deployerSigner] = await ethers.getSigners();
+    const [ deployer ] = await ethers.getSigners();
 
     const name = process.env.TOKEN_NAME;
     const symbol = process.env.TOKEN_SYMBOL;
@@ -27,7 +27,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const minAmount = process.env.MIN_AMOUNT;
 
     if (!name || !symbol || !totalGoalAmount || !minAmount) {
-        throw new Error("Missing required environment variables: TOKEN_NAME, TOKEN_SYMBOL, TOTAL_GOAL_AMOUNT, MIN_AMOUNT.");
+        throw new Error(`Missing required environment variables: TOKEN_NAME=${name}, TOKEN_SYMBOL=${symbol}, TOTAL_GOAL_AMOUNT=${totalGoalAmount}, MIN_AMOUNT=${minAmount}.`);
     }
     
     const totalGoalAmountBigInt = BigInt(totalGoalAmount);
@@ -54,7 +54,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     ];
 
     const token = await deploy("FractionalInvestmentToken", {
-        from: deployer,
+        from: deployer.address,
         args: args,
         log: true,
         waitConfirmations: network.config.blockConfirmations || 5,
